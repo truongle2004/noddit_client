ac3aac5a7f93cc64f9b76520703b5e5792e5878b
truongle2004 <119171229+truongle2004@users.noreply.github.com>
2025-05-13 19:22:45 +0700

    update create modal form

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,19 @@ import router from './router/router'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import theme from './utils/theme'
+import AuthProvider from './components/AuthProvider'
 
 const queryClient = new QueryClient()
 
 function App() {
-  // console.log(token)
-  // const auth_token = {
-  //   state: {
-  //     token: 'test'
-  //   },
-  //   version: 0
-  // }
-  // localStorage.setItem('auth-store', JSON.stringify(auth_token))
   return (
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ToastContainer />
-        <CssBaseline />
+        <AuthProvider>
+          <RouterProvider router={router} />
+          <ToastContainer />
+          <CssBaseline />
+        </AuthProvider>
       </QueryClientProvider>
     </ThemeProvider>
   )
--- src/apis/community.tsx
+++ src/apis/community.tsx
@@ -6,18 +6,18 @@ import type { Community, Response, Topic } from '@/types'
  * Get list topic
  * @returns {Promise<Topic[]>}
  */
-export const getListTopicAPI = (): Promise<Response<Topic[]>> => {
-  return axiosConfig.get(`${API_VERSION.V1}/subnoddit-service/topics`)
-}
+export const getListTopicAPI = async (): Promise<Response<Topic[]>> =>
+  await axiosConfig.get(`${API_VERSION.V1}/subnoddit-service/topics`)
 
 /**
  * Add new community with data (title, description, ...)
  * @param {FormData} formData
  * @returns {Promise<Community>}
  */
-export const addNewCommunityAPI = (formData: FormData): Promise<Community> => {
-  return axiosConfig.post(
+export const addNewCommunityAPI = async (
+  formData: FormData
+): Promise<Community> =>
+  await axiosConfig.post(
     `${API_VERSION.V1}/subnoddit-service/communities/create`,
     formData
   )
-}
